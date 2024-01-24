2017ad199ae463dd974702537e89248cb9e4dce0
Luis Waldhauser <126560323+waldluis@users.noreply.github.com>
2024-01-24 16:59:03 +0100

    Added Task3 at INF1B

---
+++ 6_Semester/INF1B/Aufgabe3/app_d.c
@@ -0,0 +1,67 @@
+
+//
+// INF1/B real time os app
+// copyright (C) 2020 by J. Krumm
+// License: Unlicense
+//
+
+#include "kernel.h"
+
+uint8_t stack0[64 * 1024];
+uint8_t stack1[64 * 1024];
+uint8_t stack2[64 * 1024];
+TCB tcb0, tcb1, tcb2;
+
+void task0() {
+	while (1) {
+		sys_idle();
+	}
+}
+
+void task1() {
+	while (1)
+	{
+		task_activate_task(&tcb2);
+		sys_idle();
+		sys_idle();
+	}
+}
+
+void task2() {
+	static int count1 = 0;
+	count1++;
+	if ((count1 % 2) == 0)//activate Task 1 every second time
+	{
+		task_activate_task(&tcb1);
+	}
+	sys_idle();
+	sys_idle();
+	task_term();
+}
+
+NAKED void app_isr() {
+	push_all_regs();
+	save_rsp();
+
+	static int count = 0;
+	count++;
+
+	if ((count % 8) == 0)//activate Task 2 every 8th time
+	{
+		isr_activate_task(&tcb2);
+	}
+
+	if (sys_get_ticks() > 100) sys_exit();
+	sys_log('I', 8);
+	task_schedule_next();
+}
+
+int app_main() {
+	task_create_task(&tcb0, stack0, sizeof(stack0), task0);
+	task_create_task(&tcb1, stack1, sizeof(stack1), task1);
+	task_create_task(&tcb2, stack2, sizeof(stack2), task2);
+	sys_enable_irq();
+
+	task_start_scheduler();
+	return 0;
+}
---
+++ 6_Semester/INF1B/Aufgabe3/kernel.h
@@ -0,0 +1,118 @@
+
+//
+// INF1/B real time os kernel header
+// copyright (C) 2020 by J. Krumm
+// License: MIT
+//
+
+#pragma once
+
+#include "sys64.h"
+
+//
+// task control block (TCB) + state flags
+//
+
+struct tcb_t {
+ uint16_t	tcb_state;
+ uint16_t	tcb_id;
+ uint8_t*	tcb_stack;
+ uint8_t*	tcb_initial_stack;
+ uint8_t*	tcb_initial_addr;
+ struct tcb_t	*tcb_next_task;
+ struct tcb_t	*tcb_next_blocked;
+ uint32_t	tcb_timeout;
+ uint32_t	tcb_ev_wait_mask;
+ uint32_t	tcb_ev_sent_mask;
+};
+
+#define		T_READY		0x00
+#define		T_RUNNING	0x10
+#define		T_WAITING	0x20
+#define		T_STOPPED	0x80
+
+#define		OK		0
+#define		E_ARG		1
+#define		E_ID		2
+#define		E_EVENTS	3
+
+#ifndef task_schedule_next
+#define task_schedule_next	task_schedule_prio
+#endif
+
+typedef struct tcb_t TCB;
+
+//
+// global internal kernel variables
+//
+
+extern TCB *g_first_task;
+extern TCB *g_this_task;
+extern TCB *g_next_timer;
+
+//
+// task handling API
+//
+
+extern void task_start_scheduler();
+extern void task_schedule();
+extern void task_dispatch();
+extern void task_reset();
+extern void task_term();
+
+extern void task_schedule_rr();
+extern void task_schedule_prio();
+
+extern void task_activate_task(TCB *task);
+extern int task_create_task(TCB *task, uint8_t *stack, int stacksize, void *entry);
+
+extern void isr_activate_task(TCB *task);
+extern void isr_task_cont();
+
+// The following inline routines are X86_64 specific
+
+INLINE void push_all_regs() { 
+ asm( "push %rax" );
+ asm( "push %rcx" );
+ asm( "push %rdx" );
+ asm( "push %rbx" );
+ asm( "push %rbp" );
+ asm( "push %rsi" );
+ asm( "push %rdi" );
+ asm( "push %r8" );
+ asm( "push %r9" );
+ asm( "push %r10" );
+ asm( "push %r11" );
+ asm( "push %r12" );
+ asm( "push %r13" );
+ asm( "push %r14" );
+ asm( "push %r15" );
+}
+
+INLINE void end_isr() {
+ asm ( "iretq" );
+}
+
+INLINE void save_rsp() {
+ asm( "mov %%rsp,%0" : "=m"(g_this_task->tcb_stack));
+}
+
+INLINE void restore_rsp() {
+ asm( "mov %0,%%rsp" :: "m"(g_this_task->tcb_stack)); 
+}
+
+//
+// template interrupt service routine
+//
+
+
+
+
+/*
+NAKED void template_isr() {
+ push_all_regs();
+ save_rsp();
+ //isr_check_timers();
+ task_schedule_next();
+}
+*/
